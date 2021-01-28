eaf33cfa80eb6a8d1ecf6a9e14c3599be1450076
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2021-01-28 10:20:27 +0530

    Get studio And customerId from pictik app

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def getStudioIdCustomerId():
        "customerId" : customerId
    }})
 
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 8000))
 
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=port)
