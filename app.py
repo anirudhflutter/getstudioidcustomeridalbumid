b9f21556369148b0b489b835db5498c5c0f5fc8c
ItFuturzTech9Teen <jagannathtech9teen@gmail.com>
2021-01-30 13:36:50 +0530

    Get studioId,customerId,albumId from pictik customer app by anirudh

--- app.py
+++ app.py
@@ -6,16 +6,19 @@ import os
 from flask import Flask, redirect, url_for, request
 app = Flask(__name__)
 
-@app.route('/getStudioIdAndCustomerId',methods = ['GET'])
-def getStudioIdCustomerId():
+@app.route('/getStudioIdCustomerIdalbumId',methods = ['GET'])
+def getStudioIdCustomerIdalbumId():
    studioId = request.args.get("studioId")
    customerId = request.args.get("customerId")
+   albumId = request.args.get("albumId")
    return jsonify({"Data" : {
        "studioId" : studioId,
-       "customerId" : customerId
+       "customerId" : customerId,
+       "albumId" : albumId
    }})
 
 port = int(os.environ.get('PORT', 8000))
 
 if __name__ == '__main__':
-   app.run(host='0.0.0.0', port=port)
+   # app.run(host='0.0.0.0', port=port)
+   app.run(debug=True)
